e24a8988f227eb4453dbc8b8e1f370fb79b06ca2
Alif Fauzan <84264896+joketoyou24@users.noreply.github.com>
2023-06-13 10:55:29 +0800

    test2

--- src/components/LoginForm/Login.jsx
+++ src/components/LoginForm/Login.jsx
@@ -21,7 +21,7 @@ const LoginForm = () => {
       ),
     };
   
-    const response = await fetch("https://rpl-production.up.railway.app/login", requestOptions);
+    const response = await fetch("process.env.REACT_APP_API_URL/login", requestOptions);
     const data = await response.json();
   
     if (!response.ok) {
